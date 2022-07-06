e10012d70f81d40ec17176952b716334353901e3
shubham-allprojects <parab16aug@gmail.com>
2022-07-06 15:57:16 +0530

    ok

--- src/7.Contact/ContactUs.js
+++ src/7.Contact/ContactUs.js
@@ -121,196 +121,217 @@ const ContactUs = () => {
   };
 
   return (
-    <motion.div>
-      <ToastContainer autoClose={2000} className="toast-container" />
-      <div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.4 }}
-        className="container mb-5 vh-100 contact"
-      >
-        <div className="text-center">
-          <h5 className="text-uppercase" style={{ color: "deeppink" }}>
-            Let's Start a Conversation
-          </h5>
-          <h1 className="fw-bold">Contact Us</h1>
+    <section className="contact">
+      <motion.div>
+        <ToastContainer autoClose={2000} className="toast-container" />
+        <div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4 }}
+          className="container"
+        >
+          <div className="text-center">
+            <h5 className="text-uppercase" style={{ color: "deeppink" }}>
+              Let's Start a Conversation
+            </h5>
+            <h1 className="fw-bold">Contact Us</h1>
+          </div>
         </div>
-        <div className="row align-items-center">
-          <div className="col-lg-6 contact-left-side">
-            <div className="address">
-              <p className="mt-lg-4 mt-2">
-                <b>Our Office</b>
-              </p>
-              <p>
-                <GrLocation />
-                <span className="mx-2">
-                  Unit No. 223, Goodwill SquareMall, Dhanori, Pune
-                </span>
-              </p>
-              <p className="bg-light">
-                <iframe
-                  className="gmap_iframe"
-                  frameborder="0"
-                  scrolling="no"
-                  marginheight="0"
-                  marginwidth="0"
-                  src="https://maps.google.com/maps?hl=en&amp;q=uvxcel&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                ></iframe>
-              </p>
-              <p>
-                <MdCall />
-                <span className="mx-2">
-                  For Enquiry: +91-20-67026259 | marketing@uvxcel.com
-                </span>
-              </p>
-              <p>
-                <BsFillEnvelopeFill />
-                <span className="mx-2">
-                  For career opportunities: hr@uvxcel.com
-                </span>
-              </p>
-              <div className="social-media mt-5">
-                <a href="https://www.linkedin.com/company/uvxcel-it-solutions/mycompany/">
-                  <BsLinkedin size={30} />
-                </a>
-                <a href="/contact" className="mx-4">
-                  <BsGithub size={30} />
-                </a>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 p-2">
+              <div className="address">
+                <h4 className="mb-4">Our Office</h4>
+                <p>
+                  <GrLocation />
+                  <span className="mx-2">
+                    Unit No. 223, Goodwill SquareMall, Dhanori, Pune
+                  </span>
+                </p>
+                <p className="bg-light">
+                  <iframe
+                    className="gmap_iframe"
+                    frameborder="0"
+                    scrolling="no"
+                    marginheight="0"
+                    marginwidth="0"
+                    src="https://maps.google.com/maps?hl=en&amp;q=uvxcel&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                  ></iframe>
+                </p>
+                <hr />
+                <p>
+                  <MdCall />
+                  <span className="mx-2">
+                    For Enquiry: +91-20-67026259 | marketing@uvxcel.com
+                  </span>
+                </p>
+                <hr />
+                <p>
+                  <BsFillEnvelopeFill />
+                  <span className="mx-2">
+                    For career opportunities: hr@uvxcel.com
+                  </span>
+                </p>
+                <hr />
+                {/* <div className="social-media mt-5">
+                  <a href="https://www.linkedin.com/company/uvxcel-it-solutions/mycompany/">
+                    <BsLinkedin size={30} />
+                  </a>
+                  <a href="/contact" className="mx-4">
+                    <BsGithub size={30} />
+                  </a>
+                </div> */}
               </div>
             </div>
-          </div>
-          <div className="col-lg-6 contact-right-side mt-5">
-            <div className="card shadow-sm p-4">
-              <form onSubmit={(e) => onSubmit(e)} className="row">
-                <div className="mb-3 col-6">
-                  <input
-                    onChange={(e) => onNameChange(e)}
-                    type="text"
-                    name="name"
-                    value={name}
-                    className="form-control"
-                    id="name"
-                    placeholder="Full Name"
-                    required
-                  />
-                  {nameErr ? (
-                    <span className="text-danger">
-                      Min 4 characters required
-                    </span>
-                  ) : (
-                    ""
-                  )}
-                </div>
-                <div className="mb-3 col-6">
-                  <input
-                    onChange={(e) => onEmailChange(e)}
-                    type="text"
-                    name="email"
-                    value={email}
-                    className="form-control"
-                    id="email"
-                    placeholder="Email"
-                    required
-                  />
-                  {emailErr ? (
-                    <span className="text-danger">Email is not Valid</span>
-                  ) : (
-                    ""
-                  )}
-                </div>
-                <div className="mb-3">
-                  <div className="input-group">
-                    <select
-                      id="mySelect"
-                      style={{ width: "180px", border: "2px solid lightblue" }}
-                      onChange={(e) => {
-                        setCountryCode(e.target.value);
-                      }}
-                      className="input-group-text btn btn-white"
-                    >
-                      {codes.map((code) => {
-                        return (
-                          <option key={code.name} value={code.code}>
-                            {code.name} {code.code}
-                          </option>
-                        );
-                      })}
-                    </select>
-                    <input
-                      onChange={(e) => onMobileChange(e)}
-                      type="text"
-                      name="mobile"
-                      value={mobile}
-                      className="form-control"
-                      id="mobile"
-                      placeholder="Enter Mobile number"
-                      required
-                    />
-                    {mobileErr ? (
+            <div className="col-md-7 p-5">
+              <form onSubmit={(e) => onSubmit(e)}>
+                <div className="row">
+                  <div className="mb-3 col-6">
+                    <div className="form-group">
+                      <input
+                        onChange={(e) => onNameChange(e)}
+                        type="text"
+                        name="name"
+                        value={name}
+                        className="form-control p-3"
+                        id="name"
+                        placeholder="Full Name"
+                        required
+                      />
+                    </div>
+                    {nameErr ? (
                       <span className="text-danger">
-                        Please enter valid mobile number
+                        Min 4 characters required
                       </span>
                     ) : (
                       ""
                     )}
                   </div>
-                </div>
-                <div className="mb-3">
-                  <input
-                    maxLength={18}
-                    onChange={(e) => onSubjectChange(e)}
-                    type="text"
-                    name="subject"
-                    value={subject}
-                    className="form-control"
-                    id="sub"
-                    placeholder="Subject"
-                    required
-                  />
-                  {subjectErr ? (
-                    <span className="text-danger">
-                      You can not leave this blank
-                    </span>
-                  ) : (
-                    ""
-                  )}
-                </div>
-                <div className="mb-3">
-                  <textarea
-                    maxLength={45}
-                    onChange={(e) => onHelpChange(e)}
-                    cols="30"
-                    name="helptext"
-                    value={helptext}
-                    rows="4"
-                    type="text"
-                    className="form-control text"
-                    id="help"
-                    placeholder="How Can We help You ?"
-                    required
-                  />
-                  {helpErr ? (
-                    <span className="text-danger">Please enter something</span>
-                  ) : (
-                    ""
-                  )}
-                </div>
-                <div className="d-flex justify-content-center">
-                  <button
-                    type="submit"
-                    id="submit-btn"
-                    className="btn btn-primary"
-                  >
-                    Submit
-                  </button>
+                  <div className="mb-3 col-6">
+                    <div className="form-group">
+                      <input
+                        onChange={(e) => onEmailChange(e)}
+                        type="text"
+                        name="email"
+                        value={email}
+                        className="form-control p-3"
+                        id="email"
+                        placeholder="Email"
+                        required
+                      />
+                    </div>
+                    {emailErr ? (
+                      <span className="text-danger">Email is not Valid</span>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <div className="col-6">
+                    <div className="form-group mb-3">
+                      <div className="input-group">
+                        <select
+                          id="mySelect"
+                          style={{
+                            width: "120px",
+                            border: "2px solid orange",
+                          }}
+                          onChange={(e) => {
+                            setCountryCode(e.target.value);
+                          }}
+                          className="input-group-text btn btn-white"
+                        >
+                          {codes.map((code) => {
+                            return (
+                              <option key={code.name} value={code.code}>
+                                {code.name} {code.code}
+                              </option>
+                            );
+                          })}
+                        </select>
+                        <input
+                          onChange={(e) => onMobileChange(e)}
+                          type="text"
+                          name="mobile"
+                          value={mobile}
+                          className="form-control p-3"
+                          id="mobile"
+                          placeholder="Enter Mobile number"
+                          required
+                        />
+                        {mobileErr ? (
+                          <span className="text-danger">
+                            Please enter valid mobile number
+                          </span>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="form-group mb-3">
+                      <input
+                        maxLength={18}
+                        onChange={(e) => onSubjectChange(e)}
+                        type="text"
+                        name="subject"
+                        value={subject}
+                        className="form-control p-3"
+                        id="sub"
+                        placeholder="Subject"
+                        required
+                      />
+                      {subjectErr ? (
+                        <span className="text-danger">
+                          You can not leave this blank
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-group mb-3">
+                      <textarea
+                        maxLength={45}
+                        onChange={(e) => onHelpChange(e)}
+                        cols="30"
+                        name="helptext"
+                        value={helptext}
+                        rows="4"
+                        type="text"
+                        className="form-control p-3 text"
+                        id="help"
+                        placeholder="How Can We help You ?"
+                        required
+                      />
+                      {helpErr ? (
+                        <span className="text-danger">
+                          Please enter something
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </div>
+                  <span className="form-group">
+                    <button
+                      style={{ width: "100%" }}
+                      type="submit"
+                      id="submit-btn"
+                      className="btn btn-primary p-2"
+                    >
+                      Send Message
+                    </button>
+                  </span>
                 </div>
               </form>
             </div>
           </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </section>
   );
 };
 
