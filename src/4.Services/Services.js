7505fb736bf8fad384dbea4a2cdaa1717a9e2bb7
shubham-allprojects <parab16aug@gmail.com>
2022-07-07 15:28:05 +0530

    ok

--- src/4.Services/Services.js
+++ src/4.Services/Services.js
@@ -12,11 +12,11 @@ const Services = () => {
         transition={{ duration: 0.4 }}
         className="container"
       >
-        <h1 className="py-4 services-ttl text-center">Our Services</h1>
+        <h1 className="services-ttl text-center">Our Services</h1>
         <div className="row justify-content-center">
           {services.map((service) => (
             <div className="col-12 col-md-6 col-lg-4" key={service.title}>
-              <div className="card shadow-sm mb-4 p-4">
+              <div className="card shadow-sm my-4 p-4">
                 <img src={service.img} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h6 className="card-title text-success">{service.title}</h6>
