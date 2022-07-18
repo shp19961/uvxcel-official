7999f4bfb49ea151136c47ade0f844c8fc6300ed
shubham-allprojects <parab16aug@gmail.com>
2022-07-18 17:29:46 +0530

    ok

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -45,4 +45,16 @@ router.get("/get-current-job/:id", async (req, res) => {
   await allJobs.findOne({ _id: req.params.id }).then((job) => res.json(job));
 });
 
+// Route-4 edit current job details
+router.put("/edit-job/:id", async (req, res) => {
+  await allJobs.findByIdAndUpdate(req.params.id, req.body);
+  res.send(req.body);
+});
+
+// Route-5 delete selected job
+router.delete("/delete-job/:id", async (req, res) => {
+  await allJobs.findByIdAndDelete(req.params.id);
+  res.send("Job deleted");
+});
+
 module.exports = router;
