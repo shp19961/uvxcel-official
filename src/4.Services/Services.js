cadc6457263e53bc91d45d7571faf2d2eb46a49d
shubham-allprojects <parab16aug@gmail.com>
2022-07-19 13:43:01 +0530

    ok

--- src/4.Services/Services.js
+++ src/4.Services/Services.js
@@ -13,14 +13,22 @@ const Services = () => {
     >
       <div className="container">
         <h1 className="services-ttl text-center">Our Services</h1>
-        <div className="row justify-content-center">
+        <div className="row">
           {services.map((service) => (
             <div className="col-12 col-md-6 col-lg-4" key={service.title}>
-              <div className="card shadow-sm my-4 p-4">
-                <img src={service.img} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h6 className="card-title text-success">{service.title}</h6>
-                  <p className="card-text">{service.parag}</p>
+              <div className="my-5">
+                <div className="row">
+                  <div className="col-4">
+                    <img src={service.img} className="card-img-top" alt="..." />
+                  </div>
+                  <div className="col-8">
+                    <div className="card-body">
+                      <h6 className="card-title text-success">
+                        {service.title}
+                      </h6>
+                      <p className="card-text">{service.parag}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
